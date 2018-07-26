d8d4e5df6cd3297197f378177ec9fdac600975cf
Jacob Lockard <jacoblockard99@gmail.com>
2018-07-26 17:30:07 -0400

    Write a new method called "ask"
    Write a new method in coder.rb that requests the user for a string and
    returns the result.

--- coder.rb
+++ coder.rb
@@ -5,4 +5,12 @@ def welcome
   puts "Welcome to the Coder app! Just type in any alphanumeric string and Coder will encrypt it for you!"
 end
 
+# Request the user for a string and return the result
+# If the input is not an alphanumeric string then the method will display a warning and request the string again
+def ask message, error = false
+  puts "Please enter letters and numbers only!" if error
+  puts message
+  gets
+end
+
 welcome
