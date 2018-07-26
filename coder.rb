99c015e7d270cc3733f1516efdf714662d1c5c69
Jacob Lockard <jacoblockard99@gmail.com>
2018-07-26 17:25:52 -0400

    Add a welcome message to coder.rb
    Modify coder.rb so that a welcome message is displayed on start up.

--- coder.rb
+++ coder.rb
@@ -1,2 +1,8 @@
 #!/bin/ruby
-puts "This program is working!"
+
+# Display a welcome message
+def welcome
+  puts "Welcome to the Coder app! Just type in any alphanumeric string and Coder will encrypt it for you!"
+end
+
+welcome
