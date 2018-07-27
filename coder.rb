52d9f5744fdf19a8bb255cdc4bcef720f26bb7ca
Jacob Lockard <jacoblockard99@gmail.com>
2018-07-27 11:07:14 -0400

    Refactor cli tools into separate file
    Refactor cli tools (ask, ask_options, etc.) into a separate file called
    (suprise!) "cli_tools.rb".

--- coder.rb
+++ coder.rb
@@ -1,54 +1,11 @@
 #!/bin/ruby
 
-class String
-  # Test if a string is alphanumeric
-  def alphanumeric?
-    return false if empty?
-    !match(/\A[a-zA-Z0-9]*\z/).nil?
-  end
-end
+require_relative 'cli_tools.rb'
 
 # Display a welcome message
 def welcome
   puts "Welcome to the Coder app! Just type in any alphanumeric string and Coder will encrypt it for you!"
 end
-
-# Request the user for a string and return the result
-def ask message
-  print "#{message} "
-  gets.chomp
-end
-alias request ask
-
-# Request the user for a string and return the result
-# If the input is not an alphanumeric string then the method will display a warning and request the string again
-def ask_alphanumeric message, error = false
-  puts "Please enter letters and numbers only!" if error
-  input = ask message
-  return input if input.alphanumeric?
-  ask_alphanumeric message, true
-end
-alias request_alphanumeric ask_alphanumeric 
-
-def ask_options message, options = ["yes", "no"], error = false
-  puts opt_msg(options) if error
-  input = ask("#{message} (#{options.join ", "})").downcase
-  return input if options.include? input
-  ask_options message, options, true
-end
-alias request_options ask_options
-
-def options_message options
-  message = "Please enter "
-  if options.count < 3
-    message += "either #{options.first} or #{options.last}!"
-  else
-    list = options.join(", ")
-    message += "one of the following: #{list}"
-  end
-  message
-end
-alias opt_msg options_message
 
 welcome
 puts ask_options "Are you sure?", ["yes", "no", "maybe"]
