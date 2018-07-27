6d76b562b5c6b3226934e3da9cc547c1237f7cb9
Jacob Lockard <jacoblockard99@gmail.com>
2018-07-27 11:13:02 -0400

    Remove welcome message

--- coder.rb
+++ coder.rb
@@ -2,11 +2,3 @@
 
 require_relative 'cli_tools.rb'
 
-# Display a welcome message
-def welcome
-  puts "Welcome to the Coder app! Just type in any alphanumeric string and Coder will encrypt it for you!"
-end
-
-welcome
-puts ask_options "Are you sure?", ["yes", "no", "maybe"]
-#input = ask_alphanumeric "What would you like to encrypt?"
