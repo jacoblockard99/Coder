e90ae971fb6454d687d0bb69c43926783d8e1e7c
Jacob Lockard <jacoblockard99@gmail.com>
2018-07-27 11:28:38 -0400

    Implement command line arguments via OptionParser

--- coder.rb
+++ coder.rb
@@ -1,4 +1,32 @@
 #!/bin/ruby
 
 require_relative 'cli_tools.rb'
+require 'optparse'
 
+options = {
+  method: :encrypt,
+  text: "example text",
+}
+
+OptionParser.new do |parser|
+  parser.banner = "Usage: coder [options]"
+
+  parser.on("-h", "--help", "Show this help message") do
+    puts parser
+  end
+
+  parser.on("-e", "--encrypt", "Encrypts text (default)") do
+  end
+
+  parser.on("-d", "--decrypt", "Decrypts text") do
+    options[:method] = :decrypt
+  end
+  
+  parser.on("-t", "--text TEXT", "The text to encrypt or decrypt") do |v|
+    options[:text] = v
+  end
+  
+  parser.on("-f", "--file PATH", "Encrypt or decrypt a file instead") do |v|
+    options[:file] = v
+  end
+end.parse!
