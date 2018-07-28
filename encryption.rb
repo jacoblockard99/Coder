e718589cacc1b5691654829bf8e80d42645c3715
Jacob Lockard <jacoblockard99@gmail.com>
2018-07-28 11:58:01 -0400

    Write encryption methods
    Write two new methods called "encrypt_string" and "decrypt_string".

--- encryption.rb
+++ encryption.rb
@@ -8,3 +8,20 @@ def shift_char char, amt, strict = false
   return char if index.nil?
   alphabet[(index + amt) % alphabet.count]
 end
+
+def encrypt_string string, key, strict = false, decrypt = false
+  alphabet = strict ? STRICT_ALPHABET : ALPHABET
+  encrypted = ""
+  string.chars.each_with_index do |char, i|
+    amt = alphabet.find_index(key.chars[i % key.length])
+    encrypted += shift_char char, decrypt ? -amt : amt, strict
+  end
+  encrypted
+end
+alias encrypt encrypt_string
+
+def decrypt_string string, key, strict = false
+  encrypt_string string, key, strict, true
+end
+alias decrypt decrypt_string
+
