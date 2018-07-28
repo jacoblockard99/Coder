fc8d4b89f9330b562759be58c178b8426b344154
Jacob Lockard <jacoblockard99@gmail.com>
2018-07-28 11:39:34 -0400

    Write a "shift_char" method
    Write a new method called "shift_char" that shifts a character up a
    given number places in the alphabet.

--- encryption.rb
+++ encryption.rb
@@ -1,2 +1,10 @@
-ALPHABET = [*("a".."z"), *(1..9), " ")]
+ALPHABET = [*("a".."z"), *(1..9), " "]
 STRICT_ALPHABET = [*("a".."z"), *("A".."Z"), *(1..9), " ", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "{", "}", "[", "]", "|", "\\", '<', '>', '.', ',', '?', '/']
+
+def shift_char char, amt, strict = false
+  char.downcase! unless strict
+  alphabet = strict ? STRICT_ALPHABET : ALPHABET
+  index = alphabet.find_index char
+  return char if index.nil?
+  alphabet[(index + amt) % alphabet.count]
+end
