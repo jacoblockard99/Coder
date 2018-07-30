8ce2297b2481adb4d82acfb448fc646661df4cbd
Jacob Lockard <jacoblockard99@gmail.com>
2018-07-30 12:38:37 -0400

    Move the APLHABET constants to "coder"
    Move the ALPHABET and STRICT_ALPHABET constant from "encryption.rb" to
    "coder".

--- encryption.rb
+++ encryption.rb
@@ -1,6 +1,3 @@
-ALPHABET = [*("a".."z"), *("1".."9"), " "]
-STRICT_ALPHABET = [*("a".."z"), *("A".."Z"), *("1".."9"), " ", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "{", "}", "[", "]", "|", "\\", '<', '>', '.', ',', '?', '/']
-
 def shift_char char, amt, strict = false
   char.downcase! unless strict
   alphabet = strict ? STRICT_ALPHABET : ALPHABET
